8a80fdeaccb6b04d41ac8f8167bb9d961fd5a4c6
EbbaSchmid <justebba@gmail.com>
2022-11-01 16:02:13 -0500

    layout

--- python_functions.py
+++ python_functions.py
@@ -1,3 +1,5 @@
+# --------- Exercise 1 ---------
+
 def sum_to(n):
 
   print(n * (n + 1) // 2)
@@ -5,10 +7,15 @@ def sum_to(n):
 sum_to(6)
 sum_to(10)
 
+# --------- Exercise 2 ---------
 
 def largest(nums):
   nums.sort()
   return nums [-1] 
 
-# print(largest ([0, 1, 4, 6, 7, 13]))
+print(largest ([0, 1, 4, 6, 7, 13]))
+
+# --------- Exercise 3 ---------
+
+
 
